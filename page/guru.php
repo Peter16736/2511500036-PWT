f7626c9d891803b781343ca1a9199273996af8b6
Peter Imanuel <2511500036@mahasiswa.atmaluhur.ac.id>
2026-04-08 18:00:45 +0700

    Dokumen

---
+++ page/guru.php
@@ -0,0 +1,67 @@
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0 text-dark">Data Guru</h1>
+      </div>
+    </div>
+  </div>
+</div>
+<?php
+if (isset($_GET['action'])) {
+    if ($_GET['action'] == "hapus") {
+        $kd = $_GET['kd'];
+        $query = mysqli_query($koneksi, "DELETE FROM guru where nip_guru = '$nip'");
+        if ($query) {
+            echo '
+            <div class="alert alert-warning alert-dismissible">
+                Berhasil Di Hapus
+            </div>';
+            echo '<meta http-equiv="refresh" content="1;url=index.php?page=guru">';
+        }
+    }
+}
+?>
+<div class="content">
+<div class="container-fluid">
+  <div class="card">
+    <div class="card-body">
+      <a href="index.php?page=tambah_guru" class="btn btn-primary btn-sm">
+        Tambah Guru
+      </a>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>NO</th>
+            <th>NIP guru</th>
+            <th>Nama guru</th>
+            <th>Aksi</th>
+          </tr>
+        <thead>
+        <?php
+        $no = 0;
+        $query = mysqli_query($koneksi, "SELECT * FROM guru");
+        while ($result = mysqli_fetch_array($query)) {
+          $no++
+        ?>  
+        <tbody>
+          <tr>
+            <td><?= $no; ?></td>
+            <td><?= $result['nip_guru']; ?></td>
+            <td><?= $result['nm_guru']; ?></td>
+            <td>
+              <a href="index.php?page=guru&action=hapus&kd=<?= $result['nip_guru'] ?>" title="">
+                <span class="badge badge-danger">Hapus</span>
+              </a>
+              <a href="index.php?page=edit_guru&kd=<?= $result['nip_guru'] ?>" title="">
+                <span class="badge badge-warning">Edit</span>
+              </a>
+            </td>
+          </tr>
+        </tbody>
+        <?php } ?>
+      </table>
+    </div>
+  </div>
+</div>
+</div>
