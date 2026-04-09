82470b53083c6a47e755c1ffdb0dd745123eb317
Peter Imanuel <2511500036@mahasiswa.atmaluhur.ac.id>
2026-04-09 21:14:35 +0700

    Dokumen

---
+++ page/edit_guru.php
@@ -0,0 +1,63 @@
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0 text-dark">Edit Guru</h1>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+$kd = $_GET['kd'];
+$edit = mysqli_fetch_array(mysqli_query($koneksi, "SELECT * FROM guru WHERE nip_guru='$nip'"));
+
+if (isset($_POST['tambah'])) {
+    $nip_guru = $_POST['nip_guru'];
+    $nm_guru = $_POST['nm_guru'];
+
+    $insert = mysqli_query($koneksi, "UPDATE guru SET nm_guru='$nm_guru' WHERE nip_guru='$nip_guru'");
+
+    if ($insert) {
+        echo '<div class="alert alert-info alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">X</button>
+                <h5><i class="icon fas fa-info"></i> Info </h5>
+                <h4>Berhasil Disimpan</h4>
+              </div>';
+        echo '<meta http-equiv="refresh" content="1;url=index.php?page=guru">';
+    } else {
+        echo '<div class="alert alert-warning alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">X</button>
+                <h5><i class="icon fas fa-info"></i> Info </h5>
+                <h4>Gagal Disimpan</h4>
+              </div>';
+    }
+}
+?>
+<section class="content">
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-body">
+                <div class="card-body p-2">
+                    <form method="post" action="">
+                        <div class="form-group">
+                            <label for="nip_guru">NIP guru</label>
+                            <input type="text" name="nip_guru" value="<?= $edit['nip_guru']; ?>" class="form-control" readonly>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="nm_guru">Nama guru</label>
+                            <input type="text" name="nm_guru" value="<?= $edit['nm_guru']; ?>" id="nm_guru" placeholder="Nama
+                                guru" class="form-control">
+                        </div>
+
+                        <div class="card-footer">
+                            <input type="submit" class="btn btn-primary" name=
+                                "tambah" value="simpan">
+                        </div>
+                    </form>
+                </div>
+             </div>
+         </div>
+    </div>
+</section>
---
+++ page/tambah_guru.php
@@ -0,0 +1,80 @@
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0 text-dark">Data Guru</h1>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+// kode otomatis
+$carikode = mysqli_query($koneksi, "select max(nip_guru) from guru") or die(mysqli_error());
+$datakode = mysqli_fetch_array($carikode);
+
+if ($datakode) {
+    $nilaikode = substr($datakode[0], 2);
+    $kode = (int) $nilaikode;
+    $kode = $kode + 1;
+    $hasilkode = "M-" . str_pad($kode, 3, "0", STR_PAD_LEFT);
+} else {
+    $hasilkode = "M-";
+}
+
+$_SESSION["KODE"] = $hasilkode;
+
+if (isset($_POST['tambah'])) {
+    $nip_guru = $_POST['nip_guru'];
+    $nm_guru = $_POST['nm_guru'];
+
+    $insert = mysqli_query($koneksi, "INSERT INTO guru VALUES ('$nip_guru','$nm_guru')");
+
+    if ($insert) {
+        echo '<div class="alert alert-info alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">X</button>
+                <h5><i class="icon fas fa-info"></i> Info </h5>
+                <h4>Berhasil Disimpan</h4>
+              </div>';
+        echo '<meta http-equiv="refresh" content="1;url=index.php?page=guru">';
+    } else {
+        echo '<div class="alert alert-warning alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">X</button>
+                <h5><i class="icon fas fa-info"></i> Info </h5>
+                <h4>Gagal Disimpan</h4>
+              </div>';
+    }
+}
+?><section class="content">
+  <div class="container-fluid">
+    <div class="card">
+      <div class="card-body">
+        <div class="card-body p-2">
+          <form method="POST" action="">
+            
+            <div class="form-group">
+              <label for="nip_guru">NIP guru</label>
+              <input type="text" name="nip_guru" 
+                     value="<?= $hasilkode; ?>" 
+                     placeholder="Id Kat" 
+                     class="form-control" readonly>
+            </div>
+
+            <div class="form-group">
+              <label for="nm_guru">Nama guru</label>
+              <input type="text" name="nm_guru" id="nm_guru" 
+                     placeholder="Nama guru" 
+                     class="form-control">
+            </div>
+
+            <div class="card-footer">
+              <input type="submit" class="btn btn-primary" 
+                     name="tambah" value="simpan">
+            </div>
+
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
